8060c063289bb82cb51ec41195cfc3fbc75b7120
girishdigge <girishdigge@gmail.com>
2024-06-02 18:44:30 +0530

    project init

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className='relative  flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
+      <div className='max-w-7xl w-full'>
+        <h1 className='text-2xl'>Howdie Hey!</h1>
+      </div>
+    </div>
+  );
+}
